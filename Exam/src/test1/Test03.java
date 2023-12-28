0b9c11e73c143c037b5086666a685943efc00bf3
qksmr5546 <qksrm5546@gmail.com>
2023-12-28 17:36:01 +0900

    Exam add

---
+++ Exam/src/test1/Test03.java
@@ -0,0 +1,29 @@
+package test1;
+
+/*
+ *  날짜 : 2023/12/28
+ *  이름 : 정태영
+ *  내용 : 자바 연산자 연습문제
+ */
+
+public class Test03 {
+	public static void main(String[] args) {
+		
+		
+		int num = 1;
+		int result;
+		
+		result = num++;
+		System.out.println("result : " + result);
+		
+		result = ++num;
+		System.out.println("result : " + result);
+		
+		result = num--;
+		System.out.println("result : " + result);
+		
+		result = --num;
+		System.out.println("result : " + result);
+		
+	}
+}
